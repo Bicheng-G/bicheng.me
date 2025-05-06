6414adc72dd9486bd28e4dfada8de2645bae5b0b
Alfie <bicheng.gu@yahoo.com>
2025-05-06 15:27:43 +0800

    chore: optimized highlight load speed

--- src/components/photos/PhotoGalleryHighlights.vue
+++ src/components/photos/PhotoGalleryHighlights.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { Photo } from '../../../photos/data'
-import raw from '../../../photos/data'
+import { onMounted, ref } from 'vue'
+// Remove direct import of getPhotosByName
+// import { getPhotosByName } from '../../../photos/data'
 
 const highlightNames: string[] = [
   'p-2022-06-19-22-18-13-000-1',
@@ -11,12 +13,23 @@ const highlightNames: string[] = [
   'p-2020-06-03-22-00-13-000-1',
 ]
 
-// Find photos by name and filter out any not found
-const photos: Photo[] = highlightNames
-  .map(name => raw.find(p => p.name === name))
-  .filter((p): p is Photo => p !== undefined)
+const photos = ref<Photo[]>([])
+
+onMounted(async () => {
+  // Dynamically import the default export (the full array)
+  const { default: allPhotos } = await import('../../../photos/data')
+  // Filter the array to get only the highlight photos
+  photos.value = highlightNames
+    .map(name => allPhotos.find(p => p.name === name))
+    .filter((p): p is Photo => p !== undefined)
+  // Previous logic using getPhotosByName removed:
+  // photos.value = await getPhotosByName(highlightNames)
+})
 </script>
 
 <template>
-  <PhotoSlide :photos="photos" />
+  <PhotoSlide v-if="photos.length" :photos="photos" />
+  <div v-else>
+    Loading photos...
+  </div>
 </template>
